511d08f3df3cba5de10c4df3f21f02c534dcb572
Sojo506 <fabiansojowork@gmail.com>
2023-05-01 10:29:46 -0600

    exercise 10 done

---
+++ exercise10.js
@@ -0,0 +1,56 @@
+/*
+Enunciado Ejercicio 10:
+Crea una función que reciba un DNI (8 números) y calcule la letra del DNI
+ 
+Es un proceso matemático facil que se basa en obtener el 
+resto de la división entera del número de DNI y el número 23. 
+Y con el resto se obtiene la letra, usandolo como posición o indice
+dentro de un array de letras.
+ 
+Este sería el array de letras:
+const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 
+                'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+ 
+Ejemplos:
+generarLetraDNI("25439343");  // Resultado: 25439343D
+ 
+*/
+
+function generateDNILetter(DNI) {
+  const letters = [
+    "T",
+    "R",
+    "W",
+    "A",
+    "G",
+    "M",
+    "Y",
+    "F",
+    "P",
+    "D",
+    "X",
+    "B",
+    "N",
+    "J",
+    "Z",
+    "S",
+    "Q",
+    "V",
+    "H",
+    "L",
+    "C",
+    "K",
+    "E",
+    "T",
+  ];
+
+  DNI = parseInt(DNI);
+
+  if (DNI < 0 && DNI > 99999999) return "Invalid DNI";
+
+  const indexLetter = letters[DNI % 23];
+
+  return DNI.toString() + indexLetter;
+}
+
+console.log(generateDNILetter("25439343"));
