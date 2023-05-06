5d48d3c527bc8a1cf52e2cf8aeb4757f8328dc5e
Sojo506 <fabiansojowork@gmail.com>
2023-05-05 21:13:51 -0600

    exercise 37 done

---
+++ exercises_31-40/exercise37.js
@@ -0,0 +1,28 @@
+/* 
+Enunciado Ejercicio 37:
+Crea dos funciones que comprueben si dado un array de números,
+todos ellos son pares.
+ 
+Una función usando metodos del lenguaje y otra solo usando estructuras de control.
+ 
+Ejemplos:
+todosPares([1, 2, 3, 4]); // false
+todosPares([2, 6, 8, 12]); // true
+*/
+
+function allPairs(arrayOfNumbers) {
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    if (arrayOfNumbers[i] % 2 !== 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function allPairs(arrayOfNumbers) {
+  return arrayOfNumbers.every((number) => number % 2 === 0);
+}
+
+console.log(allPairs([1, 2, 3, 4])); // false
+console.log(allPairs([2, 6, 8, 12])); // true
