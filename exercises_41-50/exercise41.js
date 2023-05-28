28af4048b541fe1a354297f81dfb7edbe96211a9
Sojo506 <fabiansojowork@gmail.com>
2023-05-27 18:15:04 -0600

    exercise 41 modified & done

--- exercises_41-50/exercise41.js
+++ exercises_41-50/exercise41.js
@@ -10,13 +10,13 @@ invertirEntero(-123)  // -321
 function invertInt(number) {
   const numberToString = number.toString().split("");
 
-  if (numberToString.findIndex((i) => i === "-") !== -1) {
-    return (
+  if (numberToString[0] === "-") {
+    return parseInt(
       numberToString.slice(0, 1) + numberToString.slice(1).reverse().join("")
     );
   }
 
-  return numberToString.reverse().join("");
+  return parseInt(numberToString.reverse().join(""));
 }
 
 console.log(invertInt(123)); // 321
